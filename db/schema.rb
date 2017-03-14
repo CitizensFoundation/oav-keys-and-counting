031b4a4c8e3a98bdd6fdac7e654f013dc58c950c
Robert Bjarnason <Robert Bjarnason>
2017-03-14 20:58:41 +0000

    Latest schema file

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,62 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160725111036) do
+ActiveRecord::Schema.define(version: 20161016003455) do
+
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string   "resource_id",   limit: 255,   null: false
+    t.string   "resource_type", limit: 255,   null: false
+    t.integer  "author_id",     limit: 4
+    t.string   "author_type",   limit: 255
+    t.text     "body",          limit: 65535
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "namespace",     limit: 255
+  end
+
+  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
+  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
+  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_admin_notes_on_resource_type_and_resource_id", using: :btree
+
+  create_table "admin_notes", force: :cascade do |t|
+    t.string   "resource_id",     limit: 255,   null: false
+    t.string   "resource_type",   limit: 255,   null: false
+    t.integer  "admin_user_id",   limit: 4
+    t.string   "admin_user_type", limit: 255
+    t.text     "body",            limit: 65535
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+  end
+
+  add_index "admin_notes", ["admin_user_type", "admin_user_id"], name: "index_admin_notes_on_admin_user_type_and_admin_user_id", using: :btree
+  add_index "admin_notes", ["resource_type", "resource_id"], name: "index_admin_notes_on_resource_type_and_resource_id", using: :btree
+
+  create_table "admin_users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+  end
+
+  add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
+  add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "ballots", force: :cascade do |t|
     t.binary   "areas",      limit: 65535, null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "budget_ballot_area_translations", force: :cascade do |t|
-    t.integer  "budget_ballot_area_id", limit: 4,   null: false
+    t.integer  "budget_ballot_area_id", limit: 4
     t.string   "locale",                limit: 255, null: false
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
@@ -32,12 +78,12 @@ ActiveRecord::Schema.define(version: 20160725111036) do
 
   create_table "budget_ballot_areas", force: :cascade do |t|
     t.float    "budget_amount", limit: 24
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "budget_ballot_item_translations", force: :cascade do |t|
-    t.integer  "budget_ballot_item_id", limit: 4,     null: false
+    t.integer  "budget_ballot_item_id", limit: 4
     t.string   "locale",                limit: 255,   null: false
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
@@ -48,22 +94,22 @@ ActiveRecord::Schema.define(version: 20160725111036) do
   add_index "budget_ballot_item_translations", ["budget_ballot_item_id"], name: "index_budget_ballot_item_translations_on_budget_ballot_item_id", using: :btree
   add_index "budget_ballot_item_translations", ["locale"], name: "index_budget_ballot_item_translations_on_locale", using: :btree
 
-  create_table "budget_ballots", force: :cascade do |t|
-    t.string   "letter",                limit: 255
-    t.string   "link",                  limit: 255
-    t.float    "price",                 limit: 24
-    t.integer  "idea_id",               limit: 4
-    t.integer  "budget_ballot_area_id", limit: 4
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "budget_ballot_items", force: :cascade do |t|
+    t.string   "idea_url",              limit: 255
+    t.float    "price",                 limit: 24,  null: false
+    t.integer  "idea_id",               limit: 4,   null: false
+    t.integer  "budget_ballot_area_id", limit: 4,   null: false
+    t.string   "locations",             limit: 255
+    t.string   "image_url",             limit: 255
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
   end
 
   create_table "config", force: :cascade do |t|
-    t.string   "rsk_url",                             limit: 255
+    t.string   "rsk_url",                             limit: 255,                   null: false
     t.integer  "timeout_in_seconds",                  limit: 4,                     null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.text "counting_progress"
+    t.datetime "created_at",                                                        null: false
+    t.datetime "updated_at",                                                        null: false
     t.string   "rsk_soap_username",                   limit: 255
     t.string   "rsk_soap_password",                   limit: 255
     t.string   "election_id",                         limit: 255
@@ -77,6 +123,8 @@ ActiveRecord::Schema.define(version: 20160725111036) do
     t.text     "public_key",                          limit: 65535
     t.text     "known_x509_cert",                     limit: 65535
     t.text     "areas_imagemap",                      limit: 65535
+    t.text     "counting_progress",                   limit: 65535
+    t.text     "ideas_without_pdfs",                  limit: 65535
   end
 
   create_table "final_split_votes", force: :cascade do |t|
@@ -93,8 +141,8 @@ ActiveRecord::Schema.define(version: 20160725111036) do
 
   create_table "saml_assertions", force: :cascade do |t|
     t.string   "assertion_id", limit: 255, null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   add_index "saml_assertions", ["assertion_id"], name: "index_saml_assertions_on_assertion_id", using: :btree
