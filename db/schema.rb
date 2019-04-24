53186090db23c08b779beada34debda9093f6658
Robert Bjarnason <robert.bjarnason@gmail.com>
2019-04-24 14:57:17 +0000

    Work

--- db/schema.rb
+++ db/schema.rb
@@ -122,7 +122,7 @@ ActiveRecord::Schema.define(version: 20180330170937) do
     t.string   "election_id",                         limit: 255
     t.string   "election_name",                       limit: 255
     t.text     "election_description",                limit: 65535
-    t.text     "client_config"
+    t.json    "client_config"
   end
 
   create_table "final_split_votes", force: :cascade do |t|
